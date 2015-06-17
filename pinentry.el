b3a45a51a12eff0646fe3e099854ccb1528e90b8
Daiki Ueno <ueno@unixuser.org>
2015-06-18 05:33:45 +0900

    Move the content of README to pinentry.el

--- pinentry.el
+++ pinentry.el
@@ -24,9 +24,7 @@
 ;;; Commentary:
 
 ;; This package allows GnuPG passphrase to be prompted through the
-;; minibuffer instead of graphical dialog.  As of June 2015, this
-;; feature requires newer versions of GnuPG (2.1.5 or later) and
-;; Pinentry (not yet released).
+;; minibuffer instead of graphical dialog.
 ;;
 ;; To use, add allow-emacs-pinentry to ~/.gnupg/gpg-agent.conf, and
 ;; start the server with M-x pinentry-start.
@@ -41,9 +39,14 @@
 ;;
 ;;   ${TMPDIR-/tmp}/emacs$(id -u)/pinentry
 ;;
-;; under the same directory as server.el uses.  The protocol is a
+;; under the same directory which server.el uses.  The protocol is a
 ;; subset of the Pinentry Assuan protocol described in (info
 ;; "(pinentry) Protocol").
+;;
+;; NOTE: As of June 2015, this feature requires newer versions of
+;; GnuPG (2.1.5+) and Pinentry (not yet released, possibly 0.9.5+).
+;; For details, see the discussion on gnupg-devel mailing list:
+;; <https://lists.gnupg.org/pipermail/gnupg-devel/2015-May/029875.html>.
 
 ;;; Code:
 
